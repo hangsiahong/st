15e3047bd7e74833db5929e090c7f0a2527337a8
hangsiahong <honghangsia@gmail.com>
2021-06-10 14:28:59 +0700

    new colorscheme

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "dina ttf 10px:pixelsize=34:antialias=true:autohint=true";
+static char *font = "dina ttf 10px:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 12;
 
 /*
